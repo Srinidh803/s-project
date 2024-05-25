c445d452597f4c815d439b0672652960b06af564
Srinidh803 <chsrinidhrao@gmail.com>
2024-05-25 14:58:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', function() {
+    var anibtn = document.getElementById('anibtn');
+    anibtn.style.transition = 'opacity 2s ease-in-out'; 
+
+    setTimeout(function() {
+        anibtn.style.opacity = '1'; 
+    }, 500); 
+});
+window.addEventListener('load', function() {
+    var anibtn = document.getElementById('anibh1');
+    anibtn.style.transition = 'all 2s ease-in-out'; 
+
+
+    setTimeout(function() {
+        anibtn.style.opacity = '1'; 
+    }, 500);
+});
